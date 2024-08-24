45de15a9314e72823d2f2d4975f6f5232a758fd5
Dusan Stojanovic <dusan.stojanovic@gowago.ch>
2024-08-24 14:39:30 +0200

    Removed validation.ts, questions.ts and uninstalled inquirer

--- src/validation.ts
+++
@@ -1,4 +0,0 @@
-export const isNonEmptyString = (input: string): boolean =>
-  typeof input === "string" && !!input?.length;
-
-export const isBoolean = (input: any): boolean => [true, false].includes(input);
